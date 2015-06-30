a13ae6715f7c5f0c7bfc0dab1d15032176218713
vehagn <veghag@gmail.com>
2015-06-30 13:51:47 +0200

    Increased update rate from 1 Hz to 5 Hz

--- LCDCD.cpp
+++ LCDCD.cpp
@@ -2,46 +2,46 @@
 
 int main(int argc, char* argv[]){
 	
-	if(daemon(0,0) == -1)
-		err(1, NULL);
+    if(daemon(0,0) == -1)
+        err(1, NULL);
 	  
     // targetTime in UNIX time
-	time_t targetTime = 1439190000;
+    time_t targetTime = 1439190000; //10th August 2015 9:00 a.m.
     int timeLeft = 0;
     int i, j;
 	
     //Setup LCD screen
-	hd44780 lcd(14, 15, 24, 25, 8, 7);
-   	lcd.init(20, 4);
+    hd44780 lcd(14, 15, 24, 25, 8, 7);
+    lcd.init(20, 4);
     lcd.setAutoscroll(hd44780::NO_SCROLL);
     lcd.clear(); 
     
     //Define custom symbols
     uint8_t black[8]        = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
-    lcd.defChar( hd44780::CCHAR0, black );
+    lcd.defChar(hd44780::CCHAR0, black);
     
     //Turn on backlighting
     rpihw::gpio &io = rpihw::gpio::get();
-	io.setup(23, rpihw::OUTPUT);
-	io.write(23, rpihw::HIGH); 
+    io.setup(23, rpihw::OUTPUT);
+    io.write(23, rpihw::HIGH); 
 	
     //Static text
     lcd.write("Coffee bar opens in:");
     moveAndClearLine(0,1,lcd);
-	lcd.write(" Days  Hrs  Min  Sec");
+    lcd.write(" Days  Hrs  Min  Sec");
     
-	do{       
+    do{       
         timeLeft = printTimeLeft(lcd, targetTime);
-		sleep(1);
-	} while(timeLeft);
+	usleep(2e5);
+    } while(timeLeft);
 
     //Celebrate that the countdown is finished
     lcd.clear();
     lcd.write(1,1,"The coffee bar is: ");
     lcd.write(1,2,"     !!OPEN!!      ");
     
-	for (i=0;i<360000;i++){
-        if (i%2){
+    for (i=0;i<360000;i++){
+	if (i%2){
             lcd.move( 0, 0);
             for (j=0;j<20;j++){lcd.write(hd44780::CCHAR0);}
             lcd.move( 0, 3);
@@ -50,10 +50,11 @@ int main(int argc, char* argv[]){
             lcd.write( 0, 2,hd44780::CCHAR0); lcd.write(19, 2,hd44780::CCHAR0);     
         }else{
             lcd.write( 0, 0,"                    ");
+            lcd.write( 0, 3,"                    ");
             lcd.write( 0, 1," "); lcd.write(19, 1," ");
             lcd.write( 0, 2," "); lcd.write(19, 2," ");
-            lcd.write( 0, 3,"                    ");
-        }  
-    usleep(50000);
+        }
+        (i%3)?(io.write(23, rpihw::HIGH)):(io.write(23, rpihw::LOW));
+        usleep(1e5);
     }
 }
